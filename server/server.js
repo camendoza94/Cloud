073d3e1abf193c32a5eb3279078f8dd7c7d250c4
dalthviz <d.althviz10@uniandes.edu.co>
2019-02-13 08:26:44 -0500

    Initial cron with participantRecords

--- server/server.js
+++ server/server.js
@@ -17,14 +17,13 @@ app.use(passport.initialize());
 app.use(fileUpload());
 
 const db = require('./config/db.js');
-  
+const ParticipatRecords = db.participantRecords;
+const {IN_PROGRESS} = require('./constants');
+
 // force: true will drop the table if it already exists
 db.sequelize.sync({force: true}).then(() => {
   console.log('Drop and Resync with { force: true }');
 });
-
-const {UPLOAD_PATH} = require('./constants');
-console.log(UPLOAD_PATH);
 
 // Routes
 require('./route/base.js')(app);
@@ -32,19 +31,28 @@ require('./route/user.js')(app);
 require('./route/contest.js')(app);
 require('./route/participantRecord.js')(app);
 
-// TODO: Cron to convert file
-// For now sends and email
+// For now sends an email
 cron.schedule('59 12 * * *', () => {
   console.log("---------------------");
   console.log("Running Cron Job");
-  const mail = {
-    from: 'clouddevelop2019@gmail.com',
-    to: 'clouddevelop2019@gmail.com',
-    subject: 'A new voice contest',
-    text: 'Hi there, this email was automatically sent by us for a new audio contest',
-    html: 'Hi there, this email was automatically sent by us for a new <b>audio contest</b>'
-  };
-  Emails.send(mail);
+  ParticipantRecords.findAll({where: {status: IN_PROGRESS}}).then((records)=>{
+          records.map((record) => {
+            //TODO Convert file
+            const participantEmail = record.email;
+            const mail = {
+              from: 'clouddevelop2019@gmail.com',
+              to: participantEmail,
+              subject: 'Available contests voice',
+              text: 'Hi there, this email was automatically sent by us to tell you that your voice is know available for a new audio contest',
+              html: 'Hi there, this email was automatically sent by us to tell you that your voice is know available for a new <b>audio contest</b>'
+            };
+            console.log(mail);
+            Emails.send(mail);
+          });
+
+  }).catch((err) => {
+      console.log(err.stack);
+  });
 });
 
 let listener = app.listen(process.env.PORT || 8081, function () {
