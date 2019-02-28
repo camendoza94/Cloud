8dab59c1d4991c96b0a70ba624e1bdef6b2da255
camendoza94 <camendoza94@gmail.com>
2019-02-27 23:18:48 -0500

    Change to SES

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,16 +1,44 @@
-const Emails = require('@sendgrid/mail');
-
-Emails.setApiKey(process.env.SENDGRID_API_KEY);
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'us-east-1'});
 
 // Function to send mail 'to'
 exports.sendEmail = (to) => {
-    const mail = {
-        from: 'clouddevelop2019@gmail.com',
-        to: to,
-        subject: 'Available contests voice',
-        text: 'Hi there, this email was automatically sent by us to tell you that your voice is now available for a new audio contest',
-        html: 'Hi there, this email was automatically sent by us to tell you that your voice is now available for a new <b>audio contest</b>'
+    const params = {
+        Destination: {
+            ToAddresses: [
+                to
+            ]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: "Hi there, this email was automatically sent by us to tell you that your voice is now available for a new <b>audio contest</b>"
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "Hi there, this email was automatically sent by us to tell you that your voice is now available for a new audio contest"
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'Available contests voice'
+            }
+        },
+        Source: 'clouddevelop2019@gmail.com',
+        ReplyToAddresses: [
+            'clouddevelop2019@gmail.com'
+        ]
     };
-    console.log(mail);
-    Emails.send(mail);
+
+    const sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+    sendPromise.then(
+        function(data) {
+            console.log(data.MessageId);
+        }).catch(
+        function(err) {
+            console.error(err, err.stack);
+        });
 };
