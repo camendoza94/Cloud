da3e00929b2209e95de3f3fafad45f8d35683303
dalthviz <d.althviz10@uniandes.edu.co>
2019-04-04 16:44:12 -0500

    Update routes for participantRecords

--- server/route/participantRecord.js
+++ server/route/participantRecord.js
@@ -7,11 +7,4 @@ module.exports = (app) => {
 
     app.get('/participantRecords/:id', auth.optional, ParticipantRecord.findById);
 
-    app.get('/participantRecords/:id/originalFile', auth.optional, ParticipantRecord.originalFile);
-
-    app.get('/participantRecords/:id/convertedFile', auth.optional, ParticipantRecord.convertedFile);
-
-    app.get('/participantRecords/:id/originalFile/download', auth.optional, ParticipantRecord.originalFileDownload);
-
-    app.get('/participantRecords/:id/convertedFile/download', auth.optional, ParticipantRecord.convertedFileDownload);
 };
